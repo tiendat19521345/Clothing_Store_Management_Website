260d85eb8d888dfdb895e5accfe1693e7783017f
Ho Quang Linh <hoquanglinh2710@gmail.com>
2022-05-08 23:28:15 +0700

    add page orders

--- clothing_store_management_website/src/navigation/RoutesLayout.jsx
+++ clothing_store_management_website/src/navigation/RoutesLayout.jsx
@@ -2,16 +2,17 @@ import { Switch, Route } from "react-router-dom";
 import React, { useState } from "react";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Customers from "../pages/customers/Customers";
+import Orders from "../pages/orders/Orders";
 
 const RoutesLayout = () => {
-  
-  
   return (
     <Switch>
       <Route path="/customers">
         <Customers></Customers>
       </Route>
-
+      <Route path="/orders">
+        <Orders></Orders>
+      </Route>
       <Route path="/">
         <Dashboard></Dashboard>
       </Route>
